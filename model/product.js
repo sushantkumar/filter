e6f11ad1004513b518dbe46614d9db9db1fc0dc4
Sushant <sushant@paytm.com>
2013-10-20 01:33:43 +0530

    added caching

--- model/product.js
+++ model/product.js
@@ -1,6 +1,16 @@
 "use strict";
 var mysql = require('mysql');
 var util = require('util');
+var LRU = require("lru-cache");
+
+var options = {
+    max: 500,
+    length: function(n) {
+      return n * 2;
+    },
+    maxAge: 1000 * 60 * 2
+};
+var LRU_cache = LRU(options);
 
 var connection = mysql.createConnection({
   host     : 'localhost',
@@ -19,51 +29,59 @@ var product = {
     });
   },
   byPlatform: function(platform, cb){
-    console.log(platform);
-    var i=0;
-    var params = '';
-    var plat = [];
-    for(i; i<platform.length;i++){
-      switch(platform[i].key){
-        case 'platform':
-          plat.push(platform[i].value);
-          break;
-        case 'ram':
-          var ram = platform[i].value;
-          break;
-        case 'camera':
-          var camera = platform[i].value;
-          break;
-        case 'dualcore':
-          var dualcore = platform[i].value;
-          break;
-        case 'dualsim':
-          var dualsim = platform[i].value;
-          break;
-        default:
-          break;
-      }
+    var query;
+    if(!platform){
+      query = 'select * from sample';
     }
-    console.log(plat);
-    params = params.slice(0, params.length - 3);
-    //var query = 'select * from sample where '+ params;
-    var whr = plat.length ? 'where platform in("'+plat+'")' : '';
-    if(ram != undefined)
-      whr += ' and ram > "'+ ram +'"';
-    if(camera != undefined)
-      whr += ' and camera > "'+ camera + '"';
-    if(dualsim != undefined)
-      whr += ' and dualsim = "'+ dualsim+'"';
-    if(dualcore != undefined)
-      whr += ' and dualcore = "' +dualcore+'"';
+    else {
+      var i=0;
+      var params = '';
+      var plat = [];
+      for(i; i<platform.length;i++){
+        switch(platform[i].key){
+          case 'platform':
+            plat.push(platform[i].value);
+            break;
+          case 'ram':
+            var ram = platform[i].value;
+            break;
+          case 'camera':
+            var camera = platform[i].value;
+            break;
+          case 'dualcore':
+            var dualcore = platform[i].value;
+            break;
+          case 'dualsim':
+            var dualsim = platform[i].value;
+            break;
+          default:
+            break;
+        }
+      }
+      var whr = plat.length ? 'where platform in("'+plat.join('","')+'")' : '';
+      if(ram != undefined)
+        whr += ' and ram > "'+ ram +'"';
+      if(camera != undefined)
+        whr += ' and camera > "'+ camera + '"';
+      if(dualsim != undefined)
+        whr += ' and dualsim = "'+ dualsim+'"';
+      if(dualcore != undefined)
+        whr += ' and dualcore = "' +dualcore+'"';
 
-    var query = 'select * from sample '+ whr;
-
-    console.log(query);
-
-    connection.query(query, function(err, rows) {
-      cb(err, rows);
-    });
+      query = 'select * from sample '+ whr;
+    }
+    var product = LRU_cache.get(query);
+    if(!product) {
+      connection.query(query, function(err, rows) {
+        if(!err){
+          LRU_cache.set(query, rows);
+        }
+        cb(err, rows);
+      });
+    }
+    else{
+      cb(null, product); 
+    }
   }
 
 
--- routes/user.js
+++ routes/user.js
@@ -14,42 +14,14 @@ var home = {
   platform: function(req, res, next) {
     prod.byPlatform(req.query.filter, function(err, data){
       if(!err){
-        res.json(200, data);
-      }
-    });
-  },
-  ram: function(req, res, next) {
-    prod.byRam('1', function(err, data){
-      if(!err){
-        res.json(200, data);
-      }
-    });
-  },
-  dualSim: function(req, res, next) {
-    prod.byDualsim('android', function(err, data){
-      if(!err){
-        res.json(200, data);
-      }
-    });
-  },
-  dualCore: function(req, res, next) {
-    prod.byDualcore('1', function(err, data){
-      if(!err){
-        res.json(200, data);
-      }
-    });
-  },
-  camera: function(req, res, next) {
-    prod.byCamera('android', function(err, data){
-      if(!err){
-        res.json(200, data);
-      }
-    });
-  },
-  price: function(req, res, next) {
-    prod.byPrice('10000', function(err, data){
-      if(!err){
-        res.json(200, data);
+        var products ={};
+        var html ='';
+        for(var i=0; i<data.length; i++){
+          html += "<li>"+data[i].brand + ' ' + data[i].platform 
+        }
+        products.html=html;
+        products.itemsCount = data.length;
+        res.json(200, products);
       }
     });
   }
